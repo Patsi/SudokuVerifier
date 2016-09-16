de5fc5ed63f4fc3063abd826f69431f215aa33a6
somename <someemail>
2016-09-16 15:47:27 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -3,12 +3,19 @@ public class SudokuVerifier {
 	
 	public int verify(String candidateSolution) 
 	{
+		// returns -1 if the candidate solution in not correct lengtt
 		if (candidateSolution.length() != 81)
 		{
 			return -1;
 		}
+		// returns -1 if the candidate solution does not  contain only numbers
+		else if (!candidateSolution.matches("\\d"))
+		{
+			return -1;
+		}
+		
 		// returns 0 if the candidate solution is correct
-		return 0;
+		else return 0;
 		
 		//check
 	}
