91285ea8da796977929f9ae5c03391dae964ed9a
somename <someemail>
2016-09-16 17:04:14 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -1,5 +1,6 @@
 
-public class SudokuVerifier {
+public class SudokuVerifier 
+{
 	
 	public int verify(String candidateSolution) 
 	{
@@ -8,15 +9,59 @@ public class SudokuVerifier {
 		{
 			return -1;
 		}
-		// returns -1 if the candidate solution does not  contain only numbers
+		// returns -1 if the candidate solution does not  contain only numbers 1-9
 		else if (!candidateSolution.matches("[1-9]+"))
 		{
 			return -1;
+		}
+		//return -2 if the candidate row does not contain each number exactly 1 time
+		else if (testRow(candidateSolution))
+		{
+			return -2;
+		}
+		//return -3 if the candidate collumn does not contain each number exactly 1 time
+		else if (testColumn(candidateSolution))
+		{
+			return -2;
 		}
 		
 		// returns 0 if the candidate solution is correct
 		else return 0;
 		
 		//check
+	}
+
+	private boolean testRow(String candidateString) 
+	{
+		for (int i = 0; i < 81; i = i + 9)
+		{			
+			String row = candidateString.substring(0,9);
+			if (! row.contains("1")) return false;
+			if (! row.contains("2")) return false;
+			if (! row.contains("3")) return false;
+			if (! row.contains("4")) return false;
+			if (! row.contains("5")) return false;
+			if (! row.contains("6")) return false;
+			if (! row.contains("7")) return false;
+			if (! row.contains("8")) return false;
+			if (! row.contains("9")) return false;
+		}		
+		return true;
+	}
+
+	
+	private boolean testColumn(String candidateString) 
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			String column = "";
+			for (int j = 0; j < 81; j = j + 9)
+			{
+				column = column + candidateString.substring(j + i,1);
+			}
+			// to becontinued
+		}
+		
+		return true;
 	}
 }
