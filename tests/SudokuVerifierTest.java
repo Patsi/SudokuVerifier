dc32e41e50884d6ea60b3159187d09f2fc2e71df
somename <someemail>
2016-09-16 15:24:29 +0300

    besouro automatic message

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,15 +1,27 @@
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
-public class SudokuVerifierTest {
+public class SudokuVerifierTest 
+{
 
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
 	@Test
-	public void testVerify() {
+	public void testVerify() 
+	{
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testLength0() 
+	{
+		SudokuVerifier sudokuverifier = new SudokuVerifier();
+		String candidateSolution = "";
+		
+		assertEquals(-1, sudokuverifier.verify(candidateSolution));
 	}
 
 }
